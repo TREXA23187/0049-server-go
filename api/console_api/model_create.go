36d68ad6e6ae60cdac3ee813507b4f03b9d07c4c
TREXA23187 <498111557@qq.com>
2023-07-09 21:55:26 +0100

    feat: add model file

--- api/console_api/model_create.go
+++ api/console_api/model_create.go
@@ -4,14 +4,19 @@ import (
 	"0049-server-go/global"
 	"0049-server-go/models"
 	"0049-server-go/models/res"
+	"errors"
+	"fmt"
 	"github.com/gin-gonic/gin"
+	"os"
+	"path/filepath"
 )
 
 type ModelCreateRequest struct {
-	Name       string `json:"name" binding:"required" msg:"Please enter name"`
-	CodeFile   string `json:"code_file"`
-	IsGithub   bool   `json:"is_github"`
-	GithubLink string `json:"github_link"`
+	Name           string   `json:"name" binding:"required" msg:"Please enter name"`
+	CodeFile       string   `json:"code_file"`
+	ModelFileNames []string `json:"model_file_names"`
+	IsGithub       bool     `json:"is_github"`
+	GithubLink     string   `json:"github_link"`
 }
 
 func (ConsoleApi) ModelCreateView(ctx *gin.Context) {
@@ -29,7 +34,40 @@ func (ConsoleApi) ModelCreateView(ctx *gin.Context) {
 		return
 	}
 
+	var modelFileName, modelFilePath string
+	var modelFilePaths []string
+
+	if len(cr.ModelFileNames) > 0 {
+
+		modelFilePaths = make([]string, len(cr.ModelFileNames))
+
+		for i, fileName := range cr.ModelFileNames {
+			dist := filepath.Join("uploads/model", fileName)
+			_, err := os.Stat(dist)
+			if err != nil {
+				if os.IsNotExist(err) {
+					global.Log.Error(errors.New(fmt.Sprintf("File does not exist: %s", dist)))
+					res.FailWithMessage(fmt.Sprintf("File does not exist: %s", dist), ctx)
+				} else {
+					global.Log.Error(errors.New(fmt.Sprintf("An error occurred while checking the file: %s", err)))
+					res.FailWithMessage(fmt.Sprintf("An error occurred while checking the file: %s", err), ctx)
+				}
+			}
+
+			modelFilePaths[i] = dist
+		}
+
+		modelFilePath = modelFilePaths[0]
+		modelFileName = cr.ModelFileNames[0]
+	} else {
+		modelFilePath = ""
+		modelFileName = ""
+	}
+
 	modelModel.Name = cr.Name
+	modelModel.ModelFileName = modelFileName
+	modelModel.ModelFilePath = modelFilePath
+
 	if cr.IsGithub {
 		modelModel.IsGithub = cr.IsGithub
 		modelModel.GithubLink = cr.GithubLink
--- models/model_model.go
+++ models/model_model.go
@@ -2,9 +2,10 @@ package models
 
 type ModelModel struct {
 	MODEL
-	Name       string `gorm:"size:36" json:"name"`
-	IsDefault  bool   `gorm:"size:16" json:"is_default"`
-	FilePath   string `gorm:"size:128" json:"file_path"`
-	IsGithub   bool   `gorm:"size:16" json:"is_github"`
-	GithubLink string `gorm:"size:128" json:"github_link"`
+	Name          string `gorm:"size:36" json:"name"`
+	IsDefault     bool   `gorm:"size:16" json:"is_default"`
+	ModelFileName string `gorm:"size:128" json:"model_file_name"`
+	ModelFilePath string `gorm:"size:128" json:"model_file_path"`
+	IsGithub      bool   `gorm:"size:16" json:"is_github"`
+	GithubLink    string `gorm:"size:128" json:"github_link"`
 }
