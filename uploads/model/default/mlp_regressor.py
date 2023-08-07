12e06c1a3be9b5723706f465ad7ff3f659f16527
TREXA23187 <498111557@qq.com>
2023-08-07 10:50:50 +0100

    feat: update models

--- uploads/model/default/mlp_regressor.py
+++ uploads/model/default/mlp_regressor.py
@@ -1,20 +1,41 @@
+import json
 import os
 import pickle
+import warnings
 
 import numpy as np
 import pandas as pd
 import sklearn
+from sklearn.model_selection import GridSearchCV
 from sklearn.model_selection import train_test_split
 from sklearn.neural_network import MLPRegressor
+
+warnings.filterwarnings("ignore")
 
 
 def read_dataset(data, target):
     dataset = pd.read_csv(os.path.join(os.path.dirname(__file__), "..", "data/data.csv"))
 
+    target_labels = dataset[target]
+
+    label_to_int = {label: i for i, label in enumerate(target_labels)}
+    int_to_label = {i: label for label, i in label_to_int.items()}
+
+    label_int_tag = {
+        "label_to_int": label_to_int,
+        "int_to_label": int_to_label
+    }
+    with open('model/label_int_tag.json', 'w', encoding='utf-8') as label_int_tag_file:
+        json.dump(label_int_tag, label_int_tag_file)
+
+    encoded_labels = [label_to_int[label] for label in target_labels]
+    dataset[target] = encoded_labels
+
     y = dataset[target]
     x = dataset[data]
 
     return x, y
+
 
 def split_dataset(x, y):
     return train_test_split(x, y, test_size=0.3, random_state=0)
@@ -71,8 +92,17 @@ def save_model_file(model):
     pickle.dump(model, open(model_path, "wb"))
 
 
+def advance_operate(x, y, hyper_parameters):
+    grid_search = GridSearchCV(MLPRegressor(), hyper_parameters, cv=10, scoring='accuracy')
+
+    grid_search.fit(x, y)
+
+    return grid_search.best_params_, grid_search.best_score_
+
+
 def run(config):
     x, y = read_dataset(config["dataLabel"], config["targetLabel"])
+
     x_train, x_test, y_train, y_test = split_dataset(x, y)
 
     if not os.path.exists(model_path):
@@ -83,4 +113,11 @@ def run(config):
             model = pickle.load(model_file)
 
     evaluation = evaluate_classifier_model(model, x_test, y_test)
-    return evaluation
+
+    if bool(config["hyperParameters"]):
+        optimal_hyper_parameters, optimal_accuracy = advance_operate(x, y, config["hyperParameters"])
+        advance_evaluation = {"optimal_hyper_parameters": optimal_hyper_parameters,
+                              "optimal_accuracy": optimal_accuracy}
+        return {**evaluation, **advance_evaluation}
+    else:
+        return evaluation
